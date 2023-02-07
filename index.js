1be1f156ff0ef63a68ba961c303d5bd9ab6ffd97
notvisible <sunilgowdabr1993@gmail.com>
2023-02-07 18:41:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,15 +13,6 @@ var transporter = nodemailer.createTransport({
   }
 });
 
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
-    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-});
-
 app.get('/', function(req, res) {
     console.log(req.query)
     var mailOptions = {
